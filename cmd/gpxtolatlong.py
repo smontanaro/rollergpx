f18e18abeea2e78906b417b3acfd03807f668d07
Skip Montanaro <skip.montanaro@gmail.com>
2026-01-12 13:25:29 -0600

    More typical program structure

--- cmd/gpxtolatlong.py
+++ cmd/gpxtolatlong.py
@@ -2,29 +2,55 @@
 
 "write lat/long details from GPX file to CSV format for use as course data"
 
+import argparse
 import csv
 import sys
 
 import gpx
 from haversine import haversine, Unit
 
-ride = gpx.read_gpx("/dev/stdin")
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Extract lat/long bits from a GPX file to use as course data",
+        allow_abbrev=False)
 
-writer = csv.DictWriter(sys.stdout, fieldnames="lat long dist".split())
-writer.writeheader()
+    parser.add_argument(
+        "infile", "-i", default="/dev/stdin",
+        help="GPX file from which to harvest lat/long data")
 
-ride.wpt = ride.trk[0][0]
+    parser.add_argument(
+        "outfile", "-o", default="/dev/stdout",
+        help="Generated CSV file containing lat/long/distance details for the course")
 
-pt = (ride.wpt[0].lat, ride.wpt[0].lon)
+    return parser.parse_args()
 
-writer.writerow({"lat": pt[0], "long": pt[1], "dist": 0.0})
 
-tot = 0.0
-for wpt in ride.wpt[1:]:
-    nxt = (wpt.lat, wpt.lon)
-    if nxt != pt:
-        dst = haversine(pt, nxt, unit=Unit.KILOMETERS)
-        tot += dst
-        pt = nxt
-        print(nxt, dst, tot, file=sys.stderr)
-        writer.writerow({"lat": pt[0], "long": pt[1], "dist": dst})
+def main():
+    options = parse_args()
+
+    ride = gpx.read_gpx(options.infile)
+    ride.wpt = ride.trk[0][0]
+
+    writer = csv.DictWriter(options.outfile, fieldnames="lat long dist".split())
+    writer.writeheader()
+
+    ride.wpt = ride.trk[0][0]
+
+    pt = (ride.wpt[0].lat, ride.wpt[0].lon)
+
+    writer.writerow({"lat": pt[0], "long": pt[1], "dist": 0.0})
+
+    tot = 0.0
+    for wpt in ride.wpt[1:]:
+        nxt = (wpt.lat, wpt.lon)
+        if nxt != pt:
+            dst = haversine(pt, nxt, unit=Unit.KILOMETERS)
+            tot += dst
+            pt = nxt
+            writer.writerow({"lat": pt[0], "long": pt[1], "dist": dst})
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
