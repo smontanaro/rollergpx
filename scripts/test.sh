7516a9e02412c29eab229eaaf8968702825c9eac
Skip Montanaro <skip.montanaro@gmail.com>
2026-02-06 14:52:31 -0600

    add a gpxtolatlong run for code coverage purposes, other minor tweaks

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+. $(VENVDIR)/bin/activate
+
 if [ "x$PYTHONPATH" = "x" ] ; then
-    export PYTHONPATH=$(pwd)/rollergpx
+    export PYTHONPATH=$(pwd)
 fi
 
 if [ "x${PYTHON}" = "x" ] ; then
@@ -13,23 +15,15 @@ type ${PYTHON}
 ${PYTHON} --version
 echo "+++++++++++++++++++++++++++"
 
-# Mac requires gsleep for subsecond sleeps, Linux doesn't.
-if [ "x$(which gsleep | egrep -v 'not found')" = "x" ] ; then
-    SLEEP=sleep
-else
-    SLEEP=gsleep
-fi
-
-# Mac requires gdate for %N
-if [ "x$(which gdate | egrep -v 'not found')" = "x" ] ; then
-    DATE=date
-else
-    DATE=gdate
-fi
-
 TMPGPX=$(mktemp XXXXXX.gpx)
+TMPCSV=$(mktemp XXXXXX.csv)
 
-trap "rm -f ${TMPGPX}" EXIT
+trap "rm -f ${TMPGPX} ${TMPCSV}" EXIT
+
+runcov () {
+    echo "run: $1" 1>&2
+    ${PYTHON} "$@"
+}
 
 VERBOSE=
 export DOCOVER=
@@ -54,21 +48,31 @@ while getopts 'vhc' OPTION; do
 done
 shift "$(($OPTIND -1))"
 
-coverage run -a rollergpx/rollergpx.py -v -g 5.4 < scripts/short.gpx > ${TMPGPX}
+# Run our official unit tests
+runcov $(which pytest) --cov=rollergpx $VERBOSE
+PYT=$?
+
+runcov rollergpx/rollergpx.py -v -g 5.4 < scripts/short.gpx > ${TMPGPX}
 LON=$(grep -E 'lon=' ${TMPGPX} | awk '{print $3}' | sort | uniq -c)
 if [ "${LON}" != '  56 lon="-87.65">' ] ; then
     echo "wrong output count or wrong longitude: ${LON}" 1>&2
     exit 1
 fi
-coverage run -a rollergpx/rollergpx.py -g 5.4 --course=rollergpx/data/velodrome.csv < scripts/short.gpx > ${TMPGPX}
+runcov rollergpx/rollergpx.py -g 5.4 --course=rollergpx/data/velodrome.csv < scripts/short.gpx > ${TMPGPX}
 LON=$(grep -E 'lon=' ${TMPGPX} | tail -1)
-if [ "${LON}" != '   <trkpt lat="42.12338838861973" lon="-87.81744031363048">' ] ; then
+if [ "${LON}" != '   <trkpt lat="42.12332125487012" lon="-87.81742459719388">' ] ; then
     echo "wrong ending longitude: ${LON}" 1>&2
     exit 1
 fi
 COUNT=$(grep -E 'lon=' ${TMPGPX} | wc -l)
 if [ "${COUNT}" -ne 56 ] ; then
     echo "wrong <trkpt> count: ${COUNT}" 1>&2
+    exit 1
+fi
+
+runcov rollergpx/gpxtolatlong.py < rollergpx/data/velodrome.gpx > ${TMPCSV}
+if ! cmp -s ${TMPCSV} rollergpx/data/velodrome.csv ; then
+    echo "gpxtolatlong mismatch" 1>&2
     exit 1
 fi
 
